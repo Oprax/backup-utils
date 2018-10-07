56fba831d7215da9e3320281a1ffbc2cce3bd7e8
Oprax <romuller67@hotmail.fr>
2018-10-07 06:53:52 +0100

    ci: add package + split tests and fixtures

--- tests/test_directories.py
+++ tests/test_directories.py
@@ -1,28 +1,8 @@
-import pytest
-
 from pathlib import Path
 
+from fixtures import directories_setup
+
 from backup_utils import Backup
-
-
-@pytest.yield_fixture()
-def directories_setup():
-    cfg = Path(Backup()._cfg_file)
-    cfg_bak = cfg.with_suffix(".bak")
-    if cfg_bak.exists():
-        cfg_bak.unlink()
-    cfg.rename(cfg_bak)
-    p = Path("./test_directory")
-    p.mkdir(mode=0o700, parents=True, exist_ok=True)
-    f = Path(p / "file")
-    f.write_text("lorem ipsum")
-    yield p
-    f.unlink()
-    p.rmdir()
-    cfg.unlink()
-    if cfg.exists():
-        cfg.unlink()
-    cfg_bak.rename(cfg)
 
 
 def test_directory(directories_setup):
