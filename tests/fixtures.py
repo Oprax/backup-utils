6271c182e94bd8507a9e83332419208445fe9730
Romain Muller <Romain.Muller@telemisis.com>
2018-10-15 11:48:09 +0100

    tests: add test for Task class

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -5,6 +5,25 @@ from pathlib import Path
 from backup_utils import Backup
 
 
+_cfg = {"backup": {"cmd": "cmd_test", "pre_hook": "hook1", "post_hook": "hook2"}}
+
+
+def utils_which(args):
+    return args
+
+
+def subprocess_run(*args, **kwargs):
+    from subprocess import CompletedProcess
+
+    cmd = _cfg.get("backup", {}).get("cmd", "cmd_test")
+    return CompletedProcess([cmd], 0)
+
+
+@pytest.yield_fixture()
+def config():
+    yield _cfg
+
+
 @pytest.yield_fixture()
 def directories_setup():
     cfg = Path(Backup()._cfg_file)
