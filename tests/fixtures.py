0fa9fdf02a2d7f8618322308473ab66b8f78551c
Oprax <romuller67@hotmail.fr>
2018-10-19 08:08:03 +0100

    tests: Remove unused fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -58,24 +58,3 @@ class FakeFile:
         else:
             if self._fpath.exists():
                 self._fpath.unlink()
-
-
-@pytest.yield_fixture()
-def directories_setup():
-    from backup_utils import Backup
-
-    cfg = Path(Backup()._cfg_file)
-    cfg_bak = cfg.with_suffix(".bak")
-    if cfg_bak.exists():
-        cfg_bak.unlink()
-    cfg.rename(cfg_bak)
-    p = Path("./test_directory")
-    p.mkdir(mode=0o700, parents=True, exist_ok=True)
-    f = Path(p / "file")
-    f.write_text("lorem ipsum")
-    yield p
-    f.unlink()
-    p.rmdir()
-    if cfg.exists():
-        cfg.unlink()
-    cfg_bak.rename(cfg)
--- tests/test_Backup.py
+++ tests/test_Backup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from .fixtures import config, directories_setup
+from .fixtures import config
 
 
 @pytest.yield_fixture()
