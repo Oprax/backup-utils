3a2ca9bf5a6d101410c2f05c9109da01c5700fb8
Oprax <romuller67@hotmail.fr>
2018-10-19 09:09:36 +0100

    tests: add EmailNotifier test

---
+++ tests/notifiers/test_EmailNotifier.py
@@ -0,0 +1,37 @@
+import smtplib
+from unittest.mock import patch, ANY, call
+import pytest
+from ..fixtures import config
+
+
+@pytest.yield_fixture()
+def my_cfg(config):
+    new_cfg = config.copy()
+    new_cfg.setdefault("notifier", {}).update(
+        {
+            "driver": "email",
+            "subject": "Panic",
+            "from": "backup@mg.example.org",
+            "to": "postmaster@mg.example.org",
+            "host": "smtp.mailgun.org",
+            "port": 587,
+            "login": "postmaster@mg.example.org",
+            "pswd": "p455w0rd",
+        }
+    )
+    yield new_cfg
+
+
+@patch.object(smtplib, "SMTP")
+def test_email(mock_SMTP, my_cfg):
+    from backup_utils.notifiers.EmailNotifier import EmailNotifier
+
+    cfg = dict(**my_cfg.get("notifier"))
+    t = EmailNotifier(**cfg)
+    t.send("testing email", {"testing.log": b"random data"})
+    assert mock_SMTP.mock_calls == [
+        call(cfg.get("host"), cfg.get("port")),
+        call().login(cfg.get("login"), cfg.get("pswd")),
+        call().sendmail(cfg.get("from"), cfg.get("to"), ANY),
+        call().quit()
+    ]
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,12 @@
-import sys, json
+import sys
+import json
 
 from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 
-from .fixtures import FakeFile
+from .fixtures import FakeFile, config
 
 
 @patch.object(sys, "argv", ["backup_utils", "-v"])
@@ -31,3 +32,18 @@ def test_dir():
     dirs = cfg.get("directories")
     assert len(dirs) == 1
     assert p in dirs
+
+
+# @patch.object(sys, "argv", ["backup_utils", "-r"])
+# @patch("backup_utils.databases.databases")
+# @patch("backup_utils.syncs.syncs")
+# @patch("backup_utils.tasks.tasks")
+# def test_run(mock_tasks, mock_syncs, mock_databases, config):
+#     cfg = config.copy()
+#     cfg.update({"notifier": {"driver": "print"}})
+#     with FakeFile(
+#         "~/.config/bak-utils/config.json", content=json.dumps(cfg)
+#     ) as cfg_file:
+#         from backup_utils import main
+
+#         main()
